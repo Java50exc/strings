bf1e36b87de89987a35c48381f5b7c4fb83914c3
Yuragranovsky <yuragranovsky@gmail.com>
2023-08-08 16:56:55 +0300

    HW #21 solution regex for arithmetic expression

--- src/telran/strings/Strings.java
+++ src/telran/strings/Strings.java
@@ -16,4 +16,21 @@ static public String ipV4Address() {
 	String octet = ipV4Octet();
 	return String.format("%1$s(\\.%1$s){3}", octet);
 }
+
+public static String arithmeticExpression() {
+	String operandRE = operand();
+	String operatorRE = operator();
+	return String.format("%1$s(%2$s%1$s)*",operandRE, operatorRE );
+}
+public static String operator() {
+	return "([-+*/])";
+}
+private static String operand() {
+	String numberExp = numberExp();
+	String variableExp = javaVariable();
+	return String.format("\\s*((%s|%s))\\s*", numberExp, variableExp);
+}
+private static String numberExp() {
+	return "(\\d+\\.?\\d*|\\.\\d+)";
+}
 }
--- src/telran/strings/test/StringsTest.java
+++ src/telran/strings/test/StringsTest.java
@@ -96,5 +96,25 @@ class StringsTest {
 		assertFalse("100".matches(regex));
 		assertFalse("1 2.3.4".matches(regex));
 	}
+	@Test
+	void arithmeticExpressionTrueTest() {
+		String regex = arithmeticExpression();
+		assertTrue("1.5 + a/2*10 -21".matches(regex));
+		assertTrue(" .5 + $/2* 10.0 /21.1234".matches(regex));
+		assertTrue("5. + __/2* 0.0 /0  ".matches(regex));
+		assertTrue("25.".matches(regex));
+		assertTrue("   aA123   ".matches(regex));
+	}
+	@Test
+	void arithmeticExpressionFalseTest() {
+		String regex = arithmeticExpression();
+		assertFalse("1.5 # a/2*10 -21".matches(regex));
+		assertFalse(".5 + $ 1/2* 10.0 /21.1234".matches(regex));
+		assertFalse("5. + _/2* 0.0 /0".matches(regex));
+		assertFalse("25 .".matches(regex));
+		assertFalse("aA123*".matches(regex));
+		assertFalse(" + a * b".matches(regex));
+		
+	}
 
 }
