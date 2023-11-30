ce5728f442898651a6d2fe851c599e02d7d1b5e1
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-30 19:49:42 +0200

    Update: application doesn't depend on the specific JoinString implementations

--- src/telran/performance/JoinStringsPerformanceAppl.java
+++ src/telran/performance/JoinStringsPerformanceAppl.java
@@ -1,5 +1,6 @@
 package telran.performance;
 
+import java.lang.reflect.Constructor;
 import java.util.Arrays;
 
 import telran.text.*;
@@ -8,21 +9,27 @@ public class JoinStringsPerformanceAppl {
 
 	private static final int N_STRINGS = 1000;
 	private static final int N_RUNS = 1000;
-
+	private static final String BASE_PACKAGE = "telran.text.";
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		String[] strings = getStrings();
-		PerformanceTest onBuilderTest = getTest("JoinStringsOnBuilder", strings, new JoinStringsOnBuilder());
-		PerformanceTest onStandardTest = getTest("JoinStringsOnStandard", strings, new JoinStringsOnStandard());
-		PerformanceTest onStringTest = getTest("JoinStringsOnString", strings, new JoinStringsOnString());
+		for (String className: args) {
+			try {
+				Class<JoinStrings> clazz = (Class<JoinStrings>) Class.forName(BASE_PACKAGE + className);
+				Constructor<JoinStrings> constructor = clazz.getConstructor();
+				JoinStrings joinStrings = constructor.newInstance();
+				JoinStringsPerformanceTest test =
+						new JoinStringsPerformanceTest(getTestName(clazz.getSimpleName()), N_RUNS, strings, joinStrings);
+				test.run();
+			} catch (ClassNotFoundException e) {
+				System.out.println(e);
+			} catch(Exception e) {
+				e.printStackTrace();
+				
+			}
+			
+		}
 		
-		onBuilderTest.run();
-		onStandardTest.run();
-		onStringTest.run();
-	}
-
-	private static PerformanceTest getTest(String className, String[] strings, JoinStrings joinStrings) {
-		String testName = getTestName(className);
-		return new JoinStringsPerformanceTest(testName, N_RUNS, strings, joinStrings);
 	}
 
 	private static String getTestName(String className) {
